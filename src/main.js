e2f44e593bd1240581255a650e6ab527c0bc5b6d
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-03-16 16:59:03 -0300

    arreglo error de primera entrega

--- src/main.js
+++ src/main.js
@@ -10,12 +10,14 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-new Vue({
-    render: h => h(App),
-}).$mount('#app');
-
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
+
+new Vue({
+    render: h => h(App),
+}).$mount('#app');
+
+
 
