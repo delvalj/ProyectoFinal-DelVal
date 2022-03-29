1cbc8c2d3d716cb350dd301c66f80414d29b0147
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-03-29 16:46:27 -0300

    cambio carrito

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -1,37 +1,67 @@
 <template>
-  <div class="mt-4">
-    <h4 class="text-uppercase text-center m-2 p-3">Carrito</h4>
-    <b-card
-        v-for="product in carrito" :key="product.id"
-        :img-src="product.imagenProducto"
-        img-alt="Card image"
-        img-left
-        class="mb-3 p-5 shadow-lg "
-    >
-      <b-card-title class="text-center"> {{ product.titulo }}</b-card-title>
-      <b-card-text class="text-center p-2">
-        {{ product.descripcion }}
-      </b-card-text>
 
-      <div class="quantity-toggle text-center">
-        <button @click="decrement()">&mdash;</button>
-        <input class="text-center" type="text" :value="quantity" readonly>
-        <button @click="increment()">&#xff0b;</button>
+  <div class="mt-2 row row-cols-1 row-cols-md-2 g-4">
+
+    <div>
+      <div class="m-2 p-4">
+        <b-card
+            v-for="product in carrito" :key="product.id"
+            no-body
+            class="overflow-hidden m-2 p-2"
+            style="max-width: 800px;"
+        >
+          <b-row no-gutters>
+            <b-col md="6">
+              <b-card-img :src="product.imagenProducto" alt="Image" class="rounded-0"></b-card-img>
+            </b-col>
+            <b-col md="6">
+              <b-card-body :title=product.titulo>
+                <b-card-text>
+                  {{ product.descripcion }}
+                </b-card-text>
+
+                <div class="quantity-toggle text-center">
+                  <button @click="decrement()">&mdash;</button>
+                  <input class="text-center" type="text" :value="quantity" readonly>
+                  <button @click="increment()">&#xff0b;</button>
+                </div>
+
+                <b-card-text class="text-center p-2 fw-bold mt-3">
+                  Total: ${{ product.precio * quantity }}
+                </b-card-text>
+
+                <b-card-text class="text-center m-4">
+                  <button class="h3 m-2 p-3">
+                    <b-icon icon="trash" @click="eliminarCarrito(carrito.id)"></b-icon>
+                  </button>
+                  <button class="h3 p-3 m-2">
+                    Modificar
+                  </button>
+                </b-card-text>
+              </b-card-body>
+
+            </b-col>
+          </b-row>
+
+        </b-card>
       </div>
 
-      <b-card-text class="text-center p-2 fw-bold mt-3">
-        Total: ${{ product.precio * quantity }}
-      </b-card-text>
+    </div>
+    <div>
+      <div class="m-2 p-4">
+          <b-card
+              header="Header"
+              header-tag="header"
+              footer="Card Footer"
+              footer-tag="footer"
+              title="Resumen de Compra"
+          >
+            <b-card-text>El total de tus compras es: </b-card-text>
 
-      <b-card-text class="text-center m-4">
-        <button class="h3 m-2 p-3 ">
-          <b-icon icon="trash" @click="eliminarCarrito(carrito.id)"></b-icon>
-        </button>
-        <button class="h3 p-3 m-2">
-          Modificar
-        </button>
-      </b-card-text>
-    </b-card>
+            <b-button href="#" variant="danger">Pagar</b-button>
+          </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
