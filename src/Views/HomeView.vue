1090fcf2146b99ec0d30fb07814389782f331cd1
Joaquin del VAL <delvaljoaquin@gmail.com>
2022-03-30 17:01:10 -0300

    agrego carousel en homeview

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -1,60 +1,32 @@
 <template>
   <div>
     <b-carousel
-        id="carousel-1"
-        v-model="slide"
-        :interval="4000"
-        controls
+        id="carousel-fade"
+        style="text-shadow: 0px 0px 2px #000"
+        fade
         indicators
-        background="#ababab"
         img-width="1024"
         img-height="480"
-        style="text-shadow: 1px 1px 2px #333;"
-        @sliding-start="onSlideStart"
-        @sliding-end="onSlideEnd"
+        controls
+
     >
-      <!-- Text slides with image -->
+
       <b-carousel-slide
-          caption="First slide"
-          text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-          img-src="https://picsum.photos/1024/480/?image=52"
+          caption-tag="h1"
+          caption="Bienvenidos a Nuestra Pagina"
+          img-src="https://picsum.photos/1024/480/?image=10"
       ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-              class="d-block img-fluid w-100"
-              width="1024"
-              height="480"
-              src="https://picsum.photos/1024/480/?image=55"
-              alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
+      <b-carousel-slide
+          caption-tag="body"
+          caption="Disfruta los mejores productos!"
+          img-src="https://picsum.photos/1024/480/?image=12"
+      ></b-carousel-slide>
+      <b-carousel-slide
+          caption-tag="body"
+          caption="Estamos para ayudate en lo que necesites!"
+          img-src="https://picsum.photos/1024/480/?image=22"
+      ></b-carousel-slide>
     </b-carousel>
-
-    <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p>
   </div>
 </template>
 
@@ -76,3 +48,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.carousel-caption h1 {
+  margin-bottom: 400px;
+  font-size: 80px;
+  font-family: italic;
+}
+
+
+</style>
